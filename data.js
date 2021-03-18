1d5a21cf040483071230e0095f28b98d5e9dcd31
Ja. Harochau <harochau@gmail.com>
2021-03-18 12:27:15 +0300

    smaller text for mobile

---
+++ data.js
@@ -0,0 +1,41 @@
+let n = 0,
+        p = 5;
+      dataArray = [
+        {
+          to: "https://tut.by",
+          image: "img1b.jpg",
+          title: "Заголовок 1",
+          desc:
+            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora, repellendus.",
+        },
+        {
+          to: "https://onliner.by",
+          image: "img2b.jpg",
+          title: "Заголовок 2",
+          desc: "Lorem ipsum dolor sit amet.",
+        },
+        {
+          to: "https://google.by",
+          image: "img3b.jpg",
+          title: "Заголовок 3",
+          desc: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
+        },
+        {
+          to: "https://yandex.by",
+          image: "img4b.jpg",
+          title: "Заголовок 4",
+          desc: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
+        },
+        {
+          to: "https://mail.ru",
+          image: "img5b.jpg",
+          title: "Заголовок 5",
+          desc: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
+        },
+        {
+          to: "http://ya.ru",
+          image: "img6b.jpg",
+          title: "Заголовок 6",
+          desc: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
+        },
+      ];
