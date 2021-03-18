f029050e4f408c493c70bcbc8169358c483188f0
harochau <55702755+harochau@users.noreply.github.com>
2021-03-18 03:46:16 +0300

    transition from lesson9

---
+++ menu.js
@@ -0,0 +1,7 @@
+
+let menu = document.createElement("div");
+menu.setAttribute("id","menu");
+menu.innerHTML+=`<a href="index.html">Заглавная странца</a>
+<a href="home9task1.html">Урок 9. Задание 1</a>
+<a href="home9task2.html">Урок 9. Задание 2</a>`
+document.body.appendChild(menu);
